3aa05907ad0711b728fbcd57ec7ad25956fdea49
Dinesh Yadav <dineshnirban01@gmail.com>
2024-04-17 11:23:25 +0530

    Update post.js

--- backend/routers/post.js
+++ backend/routers/post.js
@@ -19,8 +19,8 @@ const upload = multer({ storage: storage });
 
 router.post('/new', verify, async (req, res) => {
     // console.log("new post request")
-        console.log("new post request")
-    console.log(req)
+    //     console.log("new post request")
+    // console.log(req)
     const p = new post({ image: req.file.filename, description: req.body.descp, Tags: req.body.Tags, user: req.body.user, Date: req.body.date })
     await p.save()
         .then(() => {
